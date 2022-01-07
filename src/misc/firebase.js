e50986b11735cab31cc9484f598efb654ec321a9
Aditya1803 <adityatomar1803@gmail.com>
2022-01-08 02:58:48 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAsJfUUNKRaReYr04Meb5I93rL2gax7AE8',
+
+  authDomain: 'chat-app-ee819.firebaseapp.com',
+
+  projectId: 'chat-app-ee819',
+
+  storageBucket: 'chat-app-ee819.appspot.com',
+
+  messagingSenderId: '619897294289',
+
+  appId: '1:619897294289:web:eee568c70d5f31f1ba24f0',
+};
+
+const app = firebase.initializeApp(config);
